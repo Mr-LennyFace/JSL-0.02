e53380de55b42ac71c7040afcf5a4adf6e94893d
Mr-LennyFace <explorist20@gmail.com>
2021-05-19 22:23:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-9bzkqu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-9bzkqu)
---
+++ index.js
@@ -0,0 +1,38 @@
+var ex = 'Sanjay said "I love potatoes, you know"';
+console.log(ex); // Use \" for double quotations
+
+/***** 
+ \\ backslash
+ \n newline
+ \r carriage return
+ \t tab
+ \b backspace
+ \f form feed
+ *****/
+
+var mystr = 'FirstLine\nSecondLine\nThird Line';
+console.log(mystr);
+
+var family =
+  'I am the mother. ' +
+  'I am the father. ' +
+  'I am the big brother. ' +
+  'I am the small brother. ' +
+  'And this is me. ';
+
+console.log(family);
+
+var someword = 'Avacado';
+var somewordlen = 0;
+
+somewordlen = someword.length;
+console.log(somewordlen);
+
+function wordblanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = '';
+  
+
+  return result;
+}
+
+console.log(wordblanks('dog', 'big', 'ran', 'quickly'));
